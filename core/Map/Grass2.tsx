36161098b15f2f32a00ba69b02398f4ff48823a3
bctownsend829 <bctownsend1@catamount.wcu.edu>
2019-02-06 19:57:41 -0500

    hopefully fixed grass

--- core/Map/Grass2.tsx
+++ core/Map/Grass2.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="grass2" tilewidth="32" tileheight="32" tilecount="1" columns="1">
- <image source="../../../../Downloads/New Piskel.png" width="32" height="32"/>
+ <image source="../assets/grass2.png" width="32" height="32"/>
 </tileset>
