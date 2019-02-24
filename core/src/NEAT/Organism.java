b6d7e9d51bee719fe86ca99b506bf820b43db801
bctownsend829 <bc_townsend@outlook.com>
2019-02-24 14:31:09 -0500

    Maybe network?

--- core/src/NEAT/Organism.java
+++ core/src/NEAT/Organism.java
@@ -1,22 +1,34 @@
 package NEAT;
 
 public class Organism {
+    private static int id = 0;
     private double fitness;
     private String name;
     private int generation;
     private Network network;
     private Genome genome;
 
-    public Organism(double fitness,int generation){
-        this.fitness = fitness;
-        this.generation = generation;
-        this.name = "I am Groot";
+    /**
+     * Constructor for an organism based on the passed in name.
+     * @param name The name to be granted to the organism.
+     */
+    public Organism(String name){
+        this.fitness    = 0;
+        this.generation = 0;
+        this.name       = name;
+        this.network    = new Network(id);
+        id++;
+        this.genome     = new Genome();
     }
 
     public void updateNetwork(){
 
     }
 
+    /**
+     * Generates a string representation of this organism.
+     * @return A string representation of this organism.
+     */
     @Override
     public String toString() {
         return this.name + " " + this.generation + " " + this.fitness;
