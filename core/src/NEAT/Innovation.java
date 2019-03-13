1d86f93c8922fbe19042a019d5155fba004df57f
Chance Simmons <Ltcas97@gmail.com>
2019-03-13 16:02:23 -0400

    Updated species

--- core/src/NEAT/Innovation.java
+++ core/src/NEAT/Innovation.java
@@ -1,4 +1,16 @@
 package NEAT;
 
+/**
+ * Innovations are used to get rid of the possibility of adding duplicate genomes.
+ * @author Chance Simmons and Brandon Townsend
+ * @version 13 March 2019
+ */
 public class Innovation {
+
+    /**
+     * Constructor for a innovation.
+     */
+    public Innovation(){
+
+    }
 }
